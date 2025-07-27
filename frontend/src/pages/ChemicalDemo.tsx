da62ac20a78f80d5655fda645751aa30ff7ea727
pengyuyann <871774113@qq.com>
2025-07-27 21:57:01 +0800

    1

--- frontend/src/pages/ChemicalDemo.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ChemicalDisplayDemo from '../components/ChemicalDisplayDemo';
-
-const ChemicalDemo: React.FC = () => {
-  return <ChemicalDisplayDemo />;
-};
-
-export default ChemicalDemo;
